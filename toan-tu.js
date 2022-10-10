b48ed7a4c13c8387c0c9097e96b39627b799e6c8
0501my <1lanyeue@gmail.com>
2022-10-10 10:50:59 +0700

    cmt 1

---
+++ toan-tu.js
@@ -0,0 +1,6 @@
+inputWidth = prompt('nhập chiều rộng');
+inputHeight = prompt("nhập chiều cao");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("the area is: " + area);
